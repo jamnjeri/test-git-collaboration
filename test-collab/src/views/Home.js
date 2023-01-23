7ca716bd44a847957a825f8809b1d252542da4ad
zac717 <isaacongera717@gmail.com>
2023-01-23 15:59:38 +0300

    commit changes

--- test-collab/src/views/Home.js
+++ test-collab/src/views/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Jamila from '../components/Jamila'
 import Zac from './component/Zac'
 
 function Home() {
   return (
     <div>Home
       <Zac />
+      <Jamila />
     </div>
   
     
