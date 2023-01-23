8bbaa50e26c0f1b861547692a13fc7ee7669fba8
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2023-01-23 12:28:59 +0300

    sim

---
+++ test-collab/src/component/Vio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Vio() {
+  return (
+    <div>
+      violet
+    </div>
+  )
+}
+
+export default Vio
--- test-collab/src/views/Home.js
+++ test-collab/src/views/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Vio from '../component/Vio'
 
 function Home() {
   return (
+    <div>
     <div>Home</div>
+    <Vio/>
+    </div>
+    
+    
   )
 }
 
